d7c0160469ed1d22cd0cd0663a5993b14b64c041
SnowWolf68 <zhangjiashuo0818@163.com>
2024-05-27 17:23:29 +0800

    add: 接入netty的现有组件IdleStateHandler实现30秒链接没有读请求，就主动关闭链接

--- chat-server/src/main/java/com/snwolf/chat/common/websocket/NettyWebSocketServerHandler.java
+++ chat-server/src/main/java/com/snwolf/chat/common/websocket/NettyWebSocketServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -16,6 +18,15 @@ public class NettyWebSocketServerHandler extends SimpleChannelInboundHandler<Tex
     public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
         if(evt instanceof WebSocketServerProtocolHandler.HandshakeComplete){
             log.info("握手成功");
+        }else if(evt instanceof IdleStateEvent){
+            IdleStateEvent event = (IdleStateEvent) evt;
+            if(event.state() == IdleState.READER_IDLE){
+                log.info("读空闲");
+                // todo: 用户下线
+
+
+                ctx.channel().close();
+            }
         }
     }
 

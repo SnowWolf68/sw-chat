a469e914d25d12baa95e4285a2734ebd5d89b29c
SnowWolf68 <zhangjiashuo0818@163.com>
2024-05-27 22:31:39 +0800

    add: 接入微信SDK, 初步开发带参二维码接口

---
+++ chat-server/src/test/java/com/snwolf/chat/common/WxTest.java
@@ -0,0 +1,25 @@
+package com.snwolf.chat.common;
+
+import lombok.extern.slf4j.Slf4j;
+import me.chanjar.weixin.common.error.WxErrorException;
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.result.WxMpQrCodeTicket;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+@Slf4j
+public class WxTest {
+
+    @Resource
+    private WxMpService wxMpService;
+
+    @Test
+    void test() throws WxErrorException {
+        WxMpQrCodeTicket wxMpQrCodeTicket = wxMpService.getQrcodeService().qrCodeCreateTmpTicket(1, 10000);
+        String url = wxMpQrCodeTicket.getUrl();
+        log.info(url);
+    }
+}

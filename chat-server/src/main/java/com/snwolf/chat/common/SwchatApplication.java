6d7641cb8448749bd4d58d8a9ea3b7ab86d2aa0e
SnowWolf68 <zhangjiashuo0818@163.com>
2024-05-31 18:58:35 +0800

    fix: 解决同类调用中 注解不生效 的bug, 使用AopContext.currentProxy()获取代理类, 并通过代理类调用同类方法 来解决

--- chat-server/src/main/java/com/snwolf/chat/common/SwchatApplication.java
+++ chat-server/src/main/java/com/snwolf/chat/common/SwchatApplication.java
@@ -3,9 +3,11 @@ package com.snwolf.chat.common;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.snwolf.chat.common.**.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class SwchatApplication {
     public static void main(String[] args) {
         SpringApplication.run(SwchatApplication.class, args);
--- chat-server/src/main/java/com/snwolf/chat/common/user/service/impl/UserBackpackServiceImpl.java
+++ chat-server/src/main/java/com/snwolf/chat/common/user/service/impl/UserBackpackServiceImpl.java
@@ -1,6 +1,7 @@
 package com.snwolf.chat.common.user.service.impl;
 
 import cn.hutool.core.util.ObjectUtil;
+import com.snwolf.chat.common.common.annotation.RedissonLockAnno;
 import com.snwolf.chat.common.common.domain.enums.StatusEnum;
 import com.snwolf.chat.common.common.service.LockService;
 import com.snwolf.chat.common.user.dao.UserBackpackDao;
@@ -8,9 +9,11 @@ import com.snwolf.chat.common.user.domain.entity.UserBackpack;
 import com.snwolf.chat.common.user.domain.enums.IdempotentEnum;
 import com.snwolf.chat.common.user.service.IUserBackpackService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -25,26 +28,28 @@ public class UserBackpackServiceImpl implements IUserBackpackService {
     @Override
     public void distributeItem(Long uid, Long itemId, IdempotentEnum idempotentEnum, String businessId) throws Throwable {
         String idempotent = getIdempotent(itemId, idempotentEnum, businessId);
-        String redissonKey = String.format("distributeItem:%s", idempotent);
+        UserBackpackServiceImpl userBackpackService = (UserBackpackServiceImpl) AopContext.currentProxy();
+        userBackpackService.doDistributeItem(uid, itemId, idempotent);
+    }
 
-        lockService.excueteWithLock(redissonKey, () -> {
-            UserBackpack userBackpack = userBackpackDao.getByIdempotent(uid, itemId, idempotent);
-            if(ObjectUtil.isNotNull(userBackpack)){
-                // 之前获取过, 直接返回
-                log.info("用户:{}, 物品:{}, 已经发放过, idempotent:{}", uid, itemId, idempotent);
-                return;
-            }
-            // 这是第一次发放
-            // todo: 业务检查
-            // 发放物品
-            UserBackpack newUserBackPack = UserBackpack.builder()
-                    .uid(uid)
-                    .itemId(itemId)
-                    .idempotent(idempotent)
-                    .status(StatusEnum.STATUS_INVALID.getStatus())
-                    .build();
-            userBackpackDao.save(newUserBackPack);
-        });
+    @RedissonLockAnno(lockKey = "#idempotent", waitTime = 5, timeUnit = TimeUnit.SECONDS)
+    public void doDistributeItem(Long uid, Long itemId, String idempotent){
+        UserBackpack userBackpack = userBackpackDao.getByIdempotent(uid, itemId, idempotent);
+        if(ObjectUtil.isNotNull(userBackpack)){
+            // 之前获取过, 直接返回
+            log.info("用户:{}, 物品:{}, 已经发放过, idempotent:{}", uid, itemId, idempotent);
+            return;
+        }
+        // 这是第一次发放
+        // todo: 业务检查
+        // 发放物品
+        UserBackpack newUserBackPack = UserBackpack.builder()
+                .uid(uid)
+                .itemId(itemId)
+                .idempotent(idempotent)
+                .status(StatusEnum.STATUS_INVALID.getStatus())
+                .build();
+        userBackpackDao.save(newUserBackPack);
     }
 
     private String getIdempotent(Long itemId, IdempotentEnum idempotentEnum, String businessId) {

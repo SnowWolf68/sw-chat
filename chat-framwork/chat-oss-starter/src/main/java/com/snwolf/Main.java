0267057815e123844d7368557175ff942d6418ba
SnowWolf68 <zhangjiashuo0818@163.com>
2024-06-14 20:43:43 +0800

    TODO: minio模块开发

---
+++ chat-framwork/chat-oss-starter/src/main/java/com/snwolf/Main.java
@@ -0,0 +1,13 @@
+package com.snwolf;
+
+/**
+ * @author <a href="https://github.com/SnowWolf68">SnowWolf68</a>
+ * @Version: V1.0
+ * @Date: 6/14/2024
+ * @Description: ${TODO}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
